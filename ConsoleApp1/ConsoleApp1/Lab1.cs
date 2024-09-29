be8631f958652cf71e56cce0da039251a6842842
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-09-28 20:33:08 -0400

    lab commit

---
+++ ConsoleApp1/ConsoleApp1/Lab1.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+public static class Lab1
+{
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Hello COIS1020H, my name is Zannatul Tasnim Tahiti");
+    }
+}
+
+
